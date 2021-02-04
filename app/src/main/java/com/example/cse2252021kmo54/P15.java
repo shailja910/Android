abd42ad7a7ff94cc277ba7c3062fface956b6313
shailja sharma <shailjasharma91@yahoo.com>
2021-02-04 09:57:02 +0530

    P15

---
+++ app/src/main/java/com/example/cse2252021kmo54/P15.java
@@ -0,0 +1,14 @@
+package com.example.cse2252021kmo54;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class P15 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p15);
+    }
+}
